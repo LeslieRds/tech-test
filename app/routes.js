4ca0b1b9172df0ddc869ba40a871f2d23b02e0b7
Leslie Rados <leslie.rados@gmail.com>
2023-12-13 17:32:35 +0100

    erreur de route

--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,16 @@ module.exports = () => {
   const app = new express.Router();
 
   // Route pour obtenir les données des stocks
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+
   app.get("/stocks", function(req, res) {
     const data = generateStockData();
     res.json(data.stocks);
   });
 
-  app.post("/api/update", function(req, res) {
+  app.post("/update", function(req, res) {
     res.json({ message: "Données mises à jour avec succès" });
   });
 
