427fdcbe1725651b48d61adbdc3702d08a885bae
Leslie Rados <leslie.rados@gmail.com>
2023-12-13 14:51:19 +0100

    test

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,9 @@ module.exports = () => {
     res.render("index");
   });
 
+  app.post("/api/update", function(req, res) {
+    res.json({ message: "Données mises à jour avec succès" });
+  });
+
   return app;
 };
